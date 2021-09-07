b6406047b742ccadc5536a39acb306ddbd7b975f
Daniel.Yu <adam741963@gmail.com>
2021-09-07 15:30:23 -0700

    model

---
+++ tech-news-java-api/src/main/java/com/technews/VariablePractice.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 2.2;
+        double sum = a + b;
+
+        String c = "dog";
+        String contact = a + c;
+
+        System.out.println(sum);
+        System.out.println(contact);
+
+    }
+}
